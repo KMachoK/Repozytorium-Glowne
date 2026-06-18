e9f95ed25dd4516f87e1f6692a54061a34a742c8
Machowski Konrad Krzysztof <student17@VPS-IP>
2026-06-18 14:04:00 +0200

    Poprawa błędów Sphinxa: naprawa toctree i dodanie folderu _static

---
+++ source/conf.py
@@ -0,0 +1,58 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Sprawozdanie-z-laboratoriów'
+copyright = '2026, Paweł Łoćwin'
+author = 'Paweł Łoćwin'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------------
+# Optymalizacja generowania PDF - eliminacja pustych stron między rozdziałami
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+    'fncychap': '\\usepackage[Bjarne]{fncychap}',
+    'extraclassoptions': 'openany,oneside',  # KLUCZOWE: wyłącza openright i twoside
+    'preamble': r'''
+\usepackage{babel}
+\usepackage{graphicx}
+\usepackage{hyperref}
+\setcounter{tocdepth}{2}
+\raggedbottom
+
+% Zmniejszenie przestrzeni przed nagłówkami
+\setlength{\parskip}{0pt plus 1pt}
+\setlength{\parindent}{0pt}
+
+% Zwiększenie wysokości headera
+\setlength{\headheight}{14.49998pt}
+''',
+    'sphinxsetup': 'hmargin={0.7in,0.7in}, vmargin={0.7in,0.7in}, verbatimwithframe=false',
+}
+
+latex_documents = [
+    ('index', 'sprawozdanie-z-laboratoriow.tex', 'Sprawozdanie z Laboratorium: Bazy Danych', 
+     'Paweł Łoćwin', 'manual'),
+]
+
+latex_show_urls = 'footnote'
+latex_show_pagerefs = False
+latex_max_embed_pages = 0
